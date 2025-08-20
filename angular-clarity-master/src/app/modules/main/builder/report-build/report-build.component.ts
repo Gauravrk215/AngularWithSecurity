df4e85a1d9f494a4812c6df7e950f592df801774
Gaurav Kumar <GAURAVRK215@GMAIL.COM>
2025-08-20 11:41:48 +0530

    builde

---
+++ angular-clarity-master/src/app/modules/main/builder/report-build/report-build.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-build',
+  templateUrl: './report-build.component.html',
+  styleUrls: ['./report-build.component.scss']
+})
+export class ReportBuildComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
